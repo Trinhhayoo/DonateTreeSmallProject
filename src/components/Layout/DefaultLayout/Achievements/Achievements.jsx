f21d2e6d0f3262932525153125775c62570c8ed7
Ngoc Tram <ntram2k3@gmail.com>
2023-05-26 13:59:27 +0700

    Revert "Delete formDonate"
    This reverts commit 0e44bbdcac43d1f8706e7e790ca1d6444ded455f.

--- src/components/Layout/DefaultLayout/Achievements/Achievements.jsx
+++ src/components/Layout/DefaultLayout/Achievements/Achievements.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-import classNames from 'classnames/bind';
-import styles from './Achievements.module.scss';
 
-// import './Achievements.module.scss';
-const cx = classNames.bind(styles);
+import './Achievements.css';
 
 const Achievements = () => {
     return (
-        <div className={cx('wrapper')}>
+        <div>
             <p>Achievements</p>
         </div>
     );
