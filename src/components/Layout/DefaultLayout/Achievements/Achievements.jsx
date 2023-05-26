0e44bbdcac43d1f8706e7e790ca1d6444ded455f
Ngoc Tram <ntram2k3@gmail.com>
2023-05-26 12:42:08 +0700

    Delete formDonate

--- src/components/Layout/DefaultLayout/Achievements/Achievements.jsx
+++ src/components/Layout/DefaultLayout/Achievements/Achievements.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Achievements.module.scss';
 
-import './Achievements.css';
+// import './Achievements.module.scss';
+const cx = classNames.bind(styles);
 
 const Achievements = () => {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <p>Achievements</p>
         </div>
     );
