2580ba93e8a2e7c6bf6c5b97ec6e7c188286a717
Trinhhayoo <phuongtrinhh2003@gmail.com>
2023-05-28 15:51:11 +0700

    done footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React,  { useState } from "react";
 
+import mail from "../svg/2xmail.svg";
+import locate from "../svg/2xlocate.svg";
+
+
+
 
 import FormDonate from "../FormDonate/FormDonate";
 
@@ -16,8 +21,72 @@ const Footer = () => {
     return (
         <div>
           <FormDonate />
-            <footer >
-             <p>Footer</p>
+            <footer className="foter" >
+            <div className="information-container">
+                    <div className="inside-container">
+                        <div className="divide-container">
+                            <div className="logo-foter">
+                            {/* <img className="logo-footer" src={spalgTS} alt="PUN" /> */}
+                            <span></span>
+                                <h3 className = "title-project">GREENGROW</h3>
+                                
+                                <br />
+                                <br />
+                                <br />
+                                {/* <img className="fbic" src={fbic} />
+                                <img className="inic" src={inic} /> */}
+
+                            </div>
+                            <div className="lien-he">
+                                <h2 className=" title-foter">Contact</h2>
+                                <ul className="info">
+                                    <li className="each-info">
+                                        <span>
+                                            <img className=" ic-footer img-dc " src={locate} /></span>
+                                        <p className="foter-ic foter-dc">227 Nguyen Van Cu, district 5, Ho Chi Minh City</p>
+                                    </li>
+                                  
+
+                                    <li className="each-info">
+
+                                        <span>
+                                            <img className="ic-footer img-mail " src={mail} /></span>
+                                        <p className="foter-ic foter-mail">Greengrow@gmail.com</p>
+                                    </li>
+
+
+
+                                </ul>
+                            </div>
+                            <div className="Company">
+                                <h2 className="title-foter">Company</h2>
+                                <ul className="co-list">
+                                    <li className="co-li">AboutUs</li>
+                                    <li className="co-li">Values</li>
+                                    <li className="co-li">Achievements</li>
+
+                                </ul>
+
+                            </div>
+                            <div className="Resource">
+                            <h2 className="title-foter">Resources</h2>
+                            <ul className="co-list">
+                                    <li className="co-li">Projects</li>
+                                    <li className="co-li">Privacy</li>
+                                    <li className="co-li">Term of use</li>
+
+                                </ul>
+                            </div>
+                            <div className = "line" />
+                            <div className="bottom-line">
+                                <span>
+                                    <p>@Allright reserve 2022</p>
+                                </span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </footer>
         </div>
 
