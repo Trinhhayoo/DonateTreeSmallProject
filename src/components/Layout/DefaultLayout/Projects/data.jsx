347c7b6d1832454ba78279e86e0d80bb51c53c78
Trinhhayoo <phuongtrinhh2003@gmail.com>
2023-06-11 22:15:15 +0700

    add images

--- src/components/Layout/DefaultLayout/Projects/data.jsx
+++ src/components/Layout/DefaultLayout/Projects/data.jsx
@@ -1,38 +1,46 @@
-import image from  '../../../../image/plant_offer.jpg';
+import image1 from  '../../../../image/plant_offer.jpg';
+import image2 from '../../../../image/pro1.jpg';
+import image3 from '../../../../image/pro2.jpg';
+import image4 from '../../../../image/pro3.jpg';
+import image5 from '../../../../image/pro4.jpg';
+
+
+
+
 export const data = [
     {
       id: 1,
       title: 'Trees- Planting Project in Binh Phuoc',
       
       linkImg:
-      image ,
+      image1 ,
     },
     {
         id: 2,
         title: 'Trees- Planting Project in Quang Ninh',
         
         linkImg:
-        image ,
+        image2 ,
       },
     {
         id: 3,
         title: 'Trees- Planting Project in Soc Trang',
         
         linkImg:
-        image ,
+        image3 ,
       },
       {
         id: 4,
         title: 'Trees- Planting Project in Ben Tre',
         linkImg:
-        image ,
+        image4 ,
       },
       {
         id: 5,
         title: 'Trees- Planting Project in Tay Ninh',
         
         linkImg:
-        image ,
+        image5 ,
       },
    
   ];
