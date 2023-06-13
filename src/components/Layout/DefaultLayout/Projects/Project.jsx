88db4fa50c44d706c91611f157a85016a89d60fc
Ngoc Tram <ntram2k3@gmail.com>
2023-06-13 22:45:12 +0700

    review Project

--- src/components/Layout/DefaultLayout/Projects/Project.jsx
+++ src/components/Layout/DefaultLayout/Projects/Project.jsx
@@ -2,16 +2,12 @@ import styles from './Projects.module.scss';
 import classNames from 'classnames/bind';
 import logo from '../../../../svg/2xlogo.svg';
 import projectItem from '../../../../image/plant_offer.jpg';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import arrow from './arrow.svg';
 import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { dataDigitalBestSeller } from './data';
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
 
-// import { faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(styles);
 
 const images = [projectItem, projectItem, projectItem, projectItem, projectItem, projectItem];
--- src/components/Layout/DefaultLayout/Projects/data.jsx
+++ src/components/Layout/DefaultLayout/Projects/data.jsx
@@ -1,46 +1,37 @@
-import image1 from  '../../../../image/plant_offer.jpg';
+import image1 from '../../../../image/plant_offer.jpg';
 import image2 from '../../../../image/pro1.jpg';
 import image3 from '../../../../image/pro2.jpg';
 import image4 from '../../../../image/pro3.jpg';
 import image5 from '../../../../image/pro4.jpg';
 
-
-
-
-export const data = [
+export const dataDigitalBestSeller = [
     {
-      id: 1,
-      title: 'Trees- Planting Project in Binh Phuoc',
-      
-      linkImg:
-      image1 ,
+        id: 1,
+        title: 'Trees- Planting Project in Binh Phuoc',
+
+        linkImg: image1,
     },
     {
         id: 2,
         title: 'Trees- Planting Project in Quang Ninh',
-        
-        linkImg:
-        image2 ,
-      },
+
+        linkImg: image2,
+    },
     {
         id: 3,
         title: 'Trees- Planting Project in Soc Trang',
-        
-        linkImg:
-        image3 ,
-      },
-      {
+
+        linkImg: image3,
+    },
+    {
         id: 4,
         title: 'Trees- Planting Project in Ben Tre',
-        linkImg:
-        image4 ,
-      },
-      {
+        linkImg: image4,
+    },
+    {
         id: 5,
         title: 'Trees- Planting Project in Tay Ninh',
-        
-        linkImg:
-        image5 ,
-      },
-   
-  ];
+
+        linkImg: image5,
+    },
+];
