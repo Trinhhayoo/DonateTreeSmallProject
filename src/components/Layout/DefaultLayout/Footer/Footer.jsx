0602e2f7dc2b71c8a054675ef1e238ba3b1df21a
Ngoc Tram <ntram2k3@gmail.com>
2023-06-13 23:42:13 +0700

    done merge w PT

--- src/components/Layout/DefaultLayout/Footer/Footer.jsx
+++ src/components/Layout/DefaultLayout/Footer/Footer.jsx
@@ -15,60 +15,55 @@ const Footer = () => {
     return (
         <div>
             <footer className="foter">
-                <div className="information-container">
-                    <div className="inside-container">
-                        <div className="divide-container">
-                            <div className="logo-foter">
-                                {/* <img className="logo-footer" src={spalgTS} alt="PUN" /> */}
-                                <span>
-                                    <img className=" logo-footer" src={logo} />
-                                </span>
+                <div className="divide-container">
+                    <div className="symbol-footer">
+                        <img className=" logo-footer" src={logo} />
 
-                                <h3 className="title-project">GreenGrow</h3>
+                        <h3 className="title-project">GreenGrow</h3>
 
-                                <br />
-                                <br />
-                                <br />
-                            </div>
-                            <div className="lien-he">
-                                <h2 className=" title-foter">Contact</h2>
-                                <ul className="info">
-                                    <li className="each-info">
-                                        <img className="  img-dc " src={locate} />
+                        <br />
+                        <br />
+                        <br />
+                    </div>
+                    <div className="lien-he">
+                        <h2 className=" title-foter">Contact</h2>
+                        <ul className="info">
+                            <li className="each-info">
+                                <img className="  img-dc " src={locate} />
 
-                                        <p className="address-text">227 Nguyen Van Cu, district 5, Ho Chi Minh City</p>
-                                    </li>
+                                <p className="address-text">227 Nguyen Van Cu, District 5, Ho Chi Minh City</p>
+                            </li>
 
-                                    <li className="each-info">
-                                        <img className=" img-mail " src={mail} />
+                            <li className="each-info">
+                                <img className=" img-mail " src={mail} />
 
-                                        <p className="mail-text">Greengrow@gmail.com</p>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="Company">
-                                <h2 className="title-foter">Company</h2>
-                                <ul className="co-list">
-                                    <li className="co-li">AboutUs</li>
-                                    <li className="co-li">Values</li>
-                                    <li className="co-li">Achievements</li>
-                                </ul>
-                            </div>
-                            <div className="Resource">
-                                <h2 className="title-foter">Resources</h2>
-                                <ul className="co-list">
-                                    <li className="co-li">Projects</li>
-                                    <li className="co-li">Privacy</li>
-                                    <li className="co-li">Term of use</li>
-                                </ul>
-                            </div>
-                            <div className="line" />
-                            <div className="bottom-line">
-                                <p className="alright-text">@Allright reserve 2022</p>
-                                <img className=" img-fb " src={facebook} />
-                                <img className=" img-ig" src={instagram} />
-                            </div>
+                                <p className="mail-text">Greengrow@gmail.com</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="Co-Re">
+                        <div className="Company">
+                            <h2 className="title-foter">Company</h2>
+                            <ul className="co-list">
+                                <li className="co-li">AboutUs</li>
+                                <li className="co-li">Values</li>
+                                <li className="co-li">Achievements</li>
+                            </ul>
                         </div>
+                        <div className="Resource">
+                            <h2 className="title-foter">Resources</h2>
+                            <ul className="co-list">
+                                <li className="co-li">Projects</li>
+                                <li className="co-li">Privacy</li>
+                                <li className="co-li">Term of use</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="line" />
+                    <div className="bottom-line">
+                        <p className="alright-text">© Allright reserve 2022</p>
+                        <img className=" img-fb " alt="" src={facebook} />
+                        <img className=" img-ig" alt=" " src={instagram} />
                     </div>
                 </div>
             </footer>
